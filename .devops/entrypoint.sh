bd296bbd9525315d90c16f7b00ab30826c80094a
claudio-code <suporte2424@outlook.com>
2021-03-25 17:57:12 -0300

    :wrench: ajusted commands for start app

--- .devops/entrypoint.sh
+++ .devops/entrypoint.sh
@@ -1,11 +1,12 @@
-#!/bin/bash
+#!/bin/sh
 
 #On error no such file entrypoint.sh, execute in terminal - dos2unix .docker\entrypoint.sh --env=test
-# chmod -R 777 .cli
+chmod -R 777 cli
+
 chown -R www-data:www-data .
 
 composer install
 
 php bin/console server:start
 
-php bin/console doctrine:migrations:migrate --no-interaction latest
+php-fpm
