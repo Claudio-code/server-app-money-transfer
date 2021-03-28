c5b54ce1323f89cf32645ab8e073676ed17f241b
claudio-code <suporte2424@outlook.com>
2021-03-27 22:34:40 -0300

    :bulb: added swagger files

--- config/bundles.php
+++ config/bundles.php
@@ -13,6 +13,5 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
-    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
 ];
---
+++ swagger/swagger.php
@@ -0,0 +1,14 @@
+<?php
+
+use OpenApi\Annotations as OA;
+
+/**
+ * @OA\Info(
+ *     title="Money Transfer",
+ *     description="Documentação da api de transferencia de dinheiro.",
+ *     version="1.0"
+ * )
+ * @OA\Server(
+ *     url="http://localhost:8000/api/",
+ * )
+ */
