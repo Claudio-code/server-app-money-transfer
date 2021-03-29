3d3a09e8db89504133e77a6529602cb50ae007b3
claudio-code <suporte2424@outlook.com>
2021-03-29 00:32:54 -0300

    :bug: change permission

--- .devops/entrypoint.sh
+++ .devops/entrypoint.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
 #On error no such file entrypoint.sh, execute in terminal - dos2unix .docker\entrypoint.sh --env=test
-chmod -R 777 cli
+chmod -R 777 .
 
 chown -R www-data:www-data .
 
 composer install
 
 php bin/console server:start
+
+chmod -R 777 .
 
 php-fpm
